7d479849ff7f68ca127852cdb3ac3a46ba011a7e
Cleviane Rebeca <rebecacleviane@gmail.com>
2022-03-15 10:15:20 -0300

    introducing bug for challenge 1, updating challenges texts and removing unnused import

--- src/core/data/home.js
+++ src/core/data/home.js
@@ -2,12 +2,20 @@ export const acordionData = [
   {
     title: "Desafio 1",
     description:
-      "Consiste na resolução de um bug, onde uma listagem de cards de filmes deveria ser exibida, porém, por conta da ausência de controle de estado e a não utilização correta do objeto json retornado pelo TheMovieDB, o mesmo encontra-se vazio, assim não havendo conteúdo a ser percorrido e renderizado.",
+      "Consiste na resolução de um bug em que uma listagem de cards de filmes vindas da API do TheMovieDB deveria ser exibida, porém a mesma não está sendo renderizada.",
     route: "/challenge-1",
+    url: {
+      link: "https://yuujitaka.github.io/challenge/#/",
+      text: "Veja a demonstração!",
+    },
   },
   {
     title: "Desafio 2",
-    description: `Consiste na criação de um componente movieCard, e exemplo do mesmo será disponibilizado ao candidato. No processo de criação deverão ser avaliados alguns pontos, como: Organização e Código limpo.`,
+    description: `Consiste na criação de um componente MyMovieCard. No processo de criação, serão avaliados alguns pontos, como: Organização e Código limpo.`,
     route: "/challenge-2",
+    url: {
+      link: "https://www.figma.com/file/iXodmTK2h04zg44x8T8aTL/Frontend-WEB?node-id=4%3A5",
+      text: "Veja a especificação!",
+    },
   },
 ];
